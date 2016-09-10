d7eab7d3ec20ebbdca99a5511c3a90e874663d3d
Rizaldi Wahaz <rizaldi.wahaz@gmail.com>
2016-09-11 05:14:09 +0700

    Commit Kedua Menambahkan fungsi nama dan judul buku serta validasinya

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl224/formpengembalianbukupustel/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl224/formpengembalianbukupustel/MainActivity.java
@@ -2,12 +2,60 @@ package id.sch.smktelkom_mlg.tugas01.xirpl224.formpengembalianbukupustel;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNama;
+    EditText etBuku;
+    Button bOk;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etBuku = (EditText) findViewById(R.id.editTextBuku);
+        bOk = (Button) findViewById(R.id.buttonOk);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        bOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doProcess();
+            }
+        });
+
+    }
+
+    private void doProcess() {
+        if (isValid()) {
+            String nama = etNama.getText().toString();
+            String buku = etBuku.getText().toString();
+            tvHasil.setText("Nama : " + nama + "Judul Buku : " + buku);
+        }
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+        String nama = etNama.getText().toString();
+        String buku = etBuku.getText().toString();
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama Belum diisi");
+            valid = false;
+        } else if (buku.isEmpty()) {
+            etBuku.setError(" Judul Buku Belum diisi");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+
+        return valid;
     }
 }
